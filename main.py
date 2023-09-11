01395f556ecb965fcfb602319eba63f66bbd60c6
narmadhadevi1710 <144691481+narmadhadevi1710@users.noreply.github.com>
2023-09-11 18:50:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-# 1.1 Implement a recursive function to calculate the factorial of a given number.
+# Leap year
+"""
+year % 4 == 0 &
+year % 100 != 0 /
+year % 400 == 0
+
+"""
 
 
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
+def isLeapYear(year):
+  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True
   else:
-    return n * fact_rec(n - 1)
+    return False
 
 
-number = 2
-res = fact_rec(number)
+year = 2004
 
-print("The factorial of {} is {}.".format(number, res))
+if isLeapYear(year):
+  print('{} is a leap year.'.format(year))
+else:
+  print('{} is not a leap year.'.format(year))
